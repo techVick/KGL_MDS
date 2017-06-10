ae74e758c7ca065b9717150d1d28a9890ebfedba
Vikulp Sharma <svikulp@gmail.com>
2017-06-10 11:34:32 +0530

    vcommit

--- mds_s1.py
+++ mds_s1.py
@@ -9,3 +9,4 @@ test=pd.read_csv("D:/Vikulp_imp_ambitions/machine_learning/kaggle/mds/test.csv")
 train=pd.read_csv("D:/Vikulp_imp_ambitions/machine_learning/kaggle/mds/train.csv")
 sample_submission=pd.read_csv("D:/Vikulp_imp_ambitions/machine_learning/kaggle/mds/sample_submission.csv")
 
+print(test)
